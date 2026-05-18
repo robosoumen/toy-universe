41fa1c0c9df7e061f909c6e698a00db1fc0e0d67
soumen bera <soumenbera211@gmail.com>
2026-05-18 10:33:39 +0530

    Profile page css done

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { use } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import Footer from '../Components/Footer';
+import Navbar from '../Components/Navbar';
 
 const Profile = () => {
+    const {user} = use(AuthContext);
+    const {photoURL, displayName, email} = user
     return (
+       <section className='flex flex-col justify-center items-center mx-auto'>
         <div>
-            <p>profile</p>
+            <p className='text-3xl font-bold'>USER INFORMATION</p>
         </div>
+         <div>
+           <img className='w-[300px] h-[200px]' src={photoURL} alt="" />
+        </div>
+        <div>
+            <p className='text-2xl font-semibold text-red-500'>User Name : {displayName}</p>
+            <p className='text-2xl font-semibold text-red-500'>User Email : {email}</p>
+        </div>
+       </section>
     );
 };
 
